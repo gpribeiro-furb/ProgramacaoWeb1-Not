05cb43961bf4fbadf5a41b8285fb14b1c38c5fd8
elitonlunardi <lunardi.eliton@gmail.com>
2020-07-11 19:00:46 -0300

    implementacoes de listagem; add swall; não finalizado

--- TrabalhoFinal/src/pages/Employee/index.js
+++ TrabalhoFinal/src/pages/Employee/index.js
@@ -1,74 +1,113 @@
 import React, { Component } from "react";
-import Listar from "./listagem.js"
+import Listar from "./listagem.js";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import api from "../../services/api";
-
+import Swal from "sweetalert2";
 
 export default class Employee extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         name: '',
-         salary: '',
-         age: '',
-         avatar: ''
-      };
-   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+      age: "",
+      avatar: ""
+    };
+  }
 
-   handleChange = (event) => {
-      let nam = event.target.name;
-      let val = event.target.value;
-      this.setState({[nam]: val});
-   }
+  handleChange = event => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  };
 
-   handleSubmit = (event) => {
-      let data = JSON.stringify(this.state);
-      console.log(data);
-      
-      api.post("create", data).then(response => {
-         console.log(response);
+  handleSubmit = event => {
+    event.preventDefault();
+    let data = JSON.stringify(this.state);
+
+    api
+      .post("create", data)
+      .then(response => {
+        console.log(response);
+        Swal.fire("Sucesso!", "Empregado cadastrado com sucesso!", "success");
+      })
+      .catch(error => {
+        Swal.fire("Ooops :(", `${error}`, "error");
       });
-      event.preventDefault();
-   }
+  };
 
-   render() {
-      return (
-         <>
-         <Container className="d-flex flex-column justify-content-center align-items-center pt-3">
-            <Form onSubmit={this.handleSubmit}>
-               <Form.Group>
-                  <Form.Label >Nome</Form.Label>
-                  <Form.Control type="text" name="name"  placeholder="Nome" onChange={this.handleChange} value={this.state.name}/>
-               </Form.Group>
+  render() {
+    return (
+      <>
+        <Container className="d-flex flex-column justify-content-center align-items-center pt-3">
+          <Form onSubmit={this.handleSubmit}>
+            <Form.Group>
+              <Form.Label>Nome</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Nome"
+                onChange={this.handleChange}
+                value={this.state.name}
+              />
+            </Form.Group>
 
-               <Form.Row>
-                  <Form.Group as={Col}>
-                     <Form.Label >Salário</Form.Label>
-                     <Form.Control type="number" min="1" step="any" name="salary" placeholder="Salário" onChange={this.handleChange} value={this.state.salary}/>
-                  </Form.Group>
-                  <Form.Group as={Col}>
-                     <Form.Label >Idade</Form.Label>
-                      <Form.Control type="number" min="1" step="any" placeholder="Idade" name="age" onChange={this.handleChange} value={this.state.age}/>
-                  </Form.Group>
-               </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Label>Salário</Form.Label>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  step="any"
+                  name="salary"
+                  placeholder="Salário"
+                  onChange={this.handleChange}
+                  value={this.state.salary}
+                />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Idade</Form.Label>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  step="any"
+                  placeholder="Idade"
+                  name="age"
+                  onChange={this.handleChange}
+                  value={this.state.age}
+                />
+              </Form.Group>
+            </Form.Row>
 
-               <Form.Group>
-                  <Form.Label >Avatar</Form.Label>
-                  <Form.Control type="text" placeholder="Avatar" name="avatar" onChange={this.handleChange} value={this.state.avatar}/>
-               </Form.Group>
-               
-               <Button type="submit" className="mr-5" variant="primary">
-                 Salvar</Button>
-               <Button type="button" id="btnCancelar" className="mx-5" variant="danger">
-                  Cancelar
-               </Button>
-            </Form>
-         </Container>
-         <Listar/>
-         </>
-      );
-   }
+            <Form.Group>
+              <Form.Label>Avatar</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Avatar"
+                name="avatar"
+                onChange={this.handleChange}
+                value={this.state.avatar}
+              />
+            </Form.Group>
+
+            <Button type="submit" className="mr-5" variant="primary">
+              Salvar
+            </Button>
+            <Button
+              type="button"
+              id="btnCancelar"
+              className="mx-5"
+              variant="danger"
+            >
+              Cancelar
+            </Button>
+          </Form>
+        </Container>
+        <Listar />
+      </>
+    );
+  }
 }
--- TrabalhoFinal/src/pages/Employee/listagem.js
+++ TrabalhoFinal/src/pages/Employee/listagem.js
@@ -1,32 +1,71 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import api from "../../services/api";
 export default class Listar extends Component {
   constructor(props) {
-     super(props);
-     this.state = {
-        employees : [],
-        acessandoApi: true,
-     };
+    super(props);
+    this.state = {
+      employees: [],
+      acessandoApi: true,
+      novoEmpregado: null
+    };
   }
 
   async componentDidMount() {
     const response = await api.get("employees");
     this.setState(prevState => ({
-     acessandoApi : false,
-     employees: response.data.data
+      acessandoApi: false,
+      employees: response.data.data
     }));
   }
 
   render() {
-    const {acessandoApi, employees} = this.state;
-
+    const { acessandoApi, employees } = this.state;
     if (acessandoApi) {
       return <h1>Carregando...</h1>;
     } else {
-    return (
-      <>
-        <h1>Merece</h1>
-      </>
+      return (
+        <>
+          {/* id, employee_name, employee_salary, employee_age, profile_image */}
+          <table className="table table-sm mt-5">
+            <thead>
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Salário</th>
+                <th scope="col">Idade</th>
+                <th scope="col">Avatar</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map(empl => (
+                <tr key={empl.id}>
+                  <td>{empl.id}</td>
+                  <td>{empl.employee_name}</td>
+                  <td>{empl.employee_salary}</td>
+                  <td>{empl.employee_age}</td>
+                  <td>{empl.profile_image}</td>
+                  <td>
+                    <Button type="button" variant="warning" size="sm">
+                      Editar
+                    </Button>
+
+                    <Button
+                      className="ml-1"
+                      type="button"
+                      variant="danger"
+                      size="sm"
+                    >
+                      Excluir
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
       );
     }
   }
